2b8f2136bd64d74734d826772add4f6746a46c38
Joshi <joshika.a2427@gmail.com>
2026-03-25 14:29:43 +0530

    uc1

--- APP/src/BookMyStayApp.java
+++ APP/src/BookMyStayApp.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
+public class UseCaselHotelBookingApp {
 
-public class BookMyStayApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System initialized successfully.");
+    }
 }
